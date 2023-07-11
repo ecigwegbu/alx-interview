eb10acdd0057fe0a3964e3036d1fe8d44e64b260
ecigwegbu <igwegbu@gmail.com>
2023-07-11 18:32:44 +0100

    0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -25,5 +25,21 @@ def minOperations(n: int) -> int:
     H in a file, starting with a single H, and given that only copy ALL
     and paste are the only allowed operations."""
 
-    pass
-    return n
+    # Initialise operation: copy all
+    n = n           # target number of characters in the file
+    opcount = 1     # number of operations so far
+    clipboard = 1   # number of characters in the clipboard
+    file = 1        # number of characters in the file
+
+    #  while True  # loop until a breakout due to target reached
+    while (n % (file + clipboard) != 0) and (file < clipboard):
+        # paste
+        file += clipboard
+        opcount += 1
+    if (file < clipboard):
+        # paste and copy all
+        file += clipboard
+        clipboard = file
+        opcount += 2
+
+    return opcount
