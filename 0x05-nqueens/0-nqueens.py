f9cddb3c1542e5265b6854f6317cac8357a5b042
ecigwegbu <igwegbu@gmail.com>
2023-07-31 10:49:30 +0100

    n-queens setup

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""N Queens Challenge. Position N Non-attack'n queens on an NxN Chess Board"""
+from sys import argv
+
+
+def nqueens():
+    """Solve the N-queens puzzle"""
+    pass
+    return []
+
+
+if __name__ == "__main__":
+    nqueens()
